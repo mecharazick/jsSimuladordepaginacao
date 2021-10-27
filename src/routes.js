3ffbdc60921b017174c430fb522dca73d9df242b
mecharazick <lucasp@amsconsult.net>
2021-10-26 21:13:11 -0300

    Added Tests

--- src/routes.js
+++ src/routes.js
@@ -9,25 +9,46 @@ router
     res.render("index");
   })
   .post(async (req, res, next) => {
-    const { tableType, ramSize, pageQSize } = req.body;
-    const pageQ = genRandomPageRequestQueue(Number(pageQSize));
-    const delta = tableType === "wsm" ? ramSize : false;
-    const memoryManagmentUnit = new MMU({
-      ramSize: Number(ramSize),
-      pagesSize: 10,
-      delta: delta,
-    });
-    const { pageHits, pageFaults } = await memoryManagmentUnit[tableType](
-      pageQ
-    );
-    res.render("tables", {
-      tableType: tableTypeText(tableType),
-      delta: delta,
-      ramSize: ramSize,
-      pageQ: pageQ,
-      pageHits: pageHits,
-      pageFaults: pageFaults,
-    });
+    const { tableType, ramSize, pageQSize, test } = req.body;
+    if (test === "on") {
+      const pageQ = [1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5];
+      const delta = 4;
+      const memoryManagmentUnit = new MMU({
+        ramSize: 4,
+        pagesSize: 10,
+        delta: 4,
+      });
+      const { pageHits, pageFaults } = await memoryManagmentUnit[tableType](
+        pageQ
+      );
+      res.render("tables", {
+        tableType: tableTypeText(tableType),
+        delta: delta,
+        ramSize: ramSize,
+        pageQ: pageQ,
+        pageHits: pageHits,
+        pageFaults: pageFaults,
+      });
+    } else {
+      const pageQ = genRandomPageRequestQueue(Number(pageQSize));
+      const delta = tableType === "wsm" ? ramSize : false;
+      const memoryManagmentUnit = new MMU({
+        ramSize: Number(ramSize),
+        pagesSize: 10,
+        delta: delta,
+      });
+      const { pageHits, pageFaults } = await memoryManagmentUnit[tableType](
+        pageQ
+      );
+      res.render("tables", {
+        tableType: tableTypeText(tableType),
+        delta: delta,
+        ramSize: ramSize,
+        pageQ: pageQ,
+        pageHits: pageHits,
+        pageFaults: pageFaults,
+      });
+    }
 
     function tableTypeText(tableType) {
       switch (tableType) {
